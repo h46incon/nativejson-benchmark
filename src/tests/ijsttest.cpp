c5b0491dcec24a4afbfb7fcf57a7954692be7cbb
h46incon <h46incon@gmail.com>
2017-12-24 14:02:59 +0800

    Add struct define of canada.json and twitter.json

--- src/tests/ijsttest.cpp
+++ src/tests/ijsttest.cpp
@@ -1,4 +1,5 @@
 #include "../test.h"
+#include <cstring>
 
 #ifndef TEST_NAME
 #define TEST_NAME "IJST (C++)"
@@ -9,10 +10,127 @@
 #endif
 
 #include "IJST/include/ijst/ijst.h"
+#include "IJST/include/ijst/types_std.h"
 #include <iostream>
 
 using namespace ijst;
 using namespace std;
+
+//=== canada
+namespace canada {
+	IJST_DEFINE_STRUCT(
+			StGeometry
+			, (IJST_TPRI(Str), type, "type", 0)
+			, (IJST_TVEC(IJST_TVEC(IJST_TVEC(IJST_TPRI(Double)))), coordinates, "coordinates", 0)
+	);
+
+	IJST_DEFINE_STRUCT(
+			StFeature
+			, (IJST_TPRI(Str), type, "type", 0)
+			, (IJST_TMAP(IJST_TPRI(Str)), properties, "properties", 0)
+			, (IJST_TOBJ(StGeometry), geometry, "geometry", 0)
+	)
+
+	IJST_DEFINE_STRUCT(
+			StCanada
+			, (IJST_TPRI(Str), type, "type", 0)
+			, (IJST_TVEC(IJST_TOBJ(StFeature)), features, "features", 0)
+	)
+}
+typedef canada::StCanada StCanada;
+
+//=== twitter
+namespace twitter{
+	IJST_DEFINE_STRUCT(
+			StUser
+			, (IJST_TPRI(UInt64), id, "id", 0)
+			, (IJST_TPRI(Str), id_str, "id_str", 0)
+			, (IJST_TPRI(Str), screen_name, "screen_name", FDesc::Nullable)
+			, (IJST_TPRI(Str), location, "location", FDesc::Nullable)
+			, (IJST_TPRI(Str), description, "description", FDesc::Nullable)
+			, (IJST_TPRI(Str), url, "url", FDesc::Nullable)
+			, (IJST_TPRI(Raw), entities, "entities", 0)
+			, (IJST_TPRI(RBool), v_protected, "protected", 0)
+			, (IJST_TPRI(Int), followers_count, "followers_count", 0)
+			, (IJST_TPRI(Int), friends_count, "friends_count", 0)
+			, (IJST_TPRI(Int), listed_count, "listed_count", 0)
+			, (IJST_TPRI(Str), created_at, "created_at", 0)
+			, (IJST_TPRI(Int), favourites_count, "favourites_count", 0)
+			, (IJST_TPRI(Int), utc_offset, "utc_offset", FDesc::Nullable)
+			, (IJST_TPRI(Str), time_zone, "time_zone", FDesc::Nullable)
+			, (IJST_TPRI(RBool), geo_enabled, "geo_enabled", 0)
+			, (IJST_TPRI(RBool), verified, "verified", 0)
+			, (IJST_TPRI(Int), statuses_count, "statuses_count", 0)
+			, (IJST_TPRI(Str), lang, "lang", 0)
+			, (IJST_TPRI(RBool), contributors_enabled, "contributors_enabled", 0)
+			, (IJST_TPRI(RBool), is_translator, "is_translator", 0)
+			, (IJST_TPRI(RBool), is_translation_enabled, "is_translation_enabled", 0)
+			, (IJST_TPRI(Str), profile_background_color, "profile_background_color", 0)
+			, (IJST_TPRI(Str), profile_background_image_url, "profile_background_image_url", 0)
+			, (IJST_TPRI(Str), profile_background_image_url_https, "profile_background_image_url_https", 0)
+			, (IJST_TPRI(RBool), profile_background_tile, "profile_background_tile", 0)
+			, (IJST_TPRI(Str), profile_image_url, "profile_image_url", 0)
+			, (IJST_TPRI(Str), profile_image_url_https, "profile_image_url_https", 0)
+			, (IJST_TPRI(Str), profile_banner_url, "profile_banner_url", FDesc::Optional)
+			, (IJST_TPRI(Str), profile_link_color, "profile_link_color", 0)
+			, (IJST_TPRI(Str), profile_sidebar_border_color, "profile_sidebar_border_color", 0)
+			, (IJST_TPRI(Str), profile_sidebar_fill_color, "profile_sidebar_fill_color", 0)
+			, (IJST_TPRI(Str), profile_text_color, "profile_text_color", 0)
+			, (IJST_TPRI(RBool), profile_use_background_image, "profile_use_background_image", 0)
+			, (IJST_TPRI(RBool), default_profile, "default_profile", 0)
+			, (IJST_TPRI(RBool), default_profile_image, "default_profile_image", 0)
+			, (IJST_TPRI(RBool), following, "following", 0)
+			, (IJST_TPRI(RBool), follow_request_sent, "follow_request_sent", 0)
+			, (IJST_TPRI(RBool), notifications, "notifications", 0)
+	)
+
+	IJST_DEFINE_STRUCT(
+			StStatus
+			, (IJST_TMAP(IJST_TPRI(Str)), metadata, "metadata", 0)
+			, (IJST_TPRI(Str), created_at, "created_at", 0)
+			, (IJST_TPRI(UInt64), id, "id", 0)
+			, (IJST_TPRI(Str), id_str, "id_str", 0)
+			, (IJST_TPRI(Str), text, "text", 0)
+			, (IJST_TPRI(Str), source, "source", 0)
+			, (IJST_TPRI(RBool), truncated, "truncated", 0)
+			, (IJST_TPRI(UInt64), in_reply_to_status_id, "in_reply_to_status_id", FDesc::Nullable)
+			, (IJST_TPRI(Str), in_reply_to_status_id_str, "in_reply_to_status_id_str", FDesc::Nullable)
+			, (IJST_TPRI(UInt64), in_reply_to_user_id, "in_reply_to_user_id", FDesc::Nullable)
+			, (IJST_TPRI(Str), in_reply_to_user_id_str, "in_reply_to_user_id_str", FDesc::Nullable)
+			, (IJST_TPRI(Str), in_reply_to_screen_name, "in_reply_to_screen_name", FDesc::Nullable)
+			, (IJST_TOBJ(StUser), user, "user", 0)
+			, (IJST_TPRI(Str), geo, "geo", FDesc::Nullable)
+			, (IJST_TPRI(Str), coordinates, "coordinates", FDesc::Nullable)
+			, (IJST_TPRI(Str), place, "place", FDesc::Nullable)
+			, (IJST_TPRI(Str), contributors, "contributors", FDesc::Nullable)
+			, (IJST_TPRI(Int), retweet_count, "retweet_count", 0)
+			, (IJST_TPRI(Int), favorite_count, "favorite_count", 0)
+			, (IJST_TPRI(Raw), entities, "entities", 0)
+			, (IJST_TPRI(RBool), favorited, "favorited", 0)
+			, (IJST_TPRI(RBool), retweeted, "retweeted", 0)
+			, (IJST_TPRI(Str), lang, "lang", 0)
+	)
+
+	IJST_DEFINE_STRUCT(
+			StSearchMetaData
+			, (IJST_TPRI(Double), completed_in, "completed_in", 0)
+			, (IJST_TPRI(UInt64), max_id, "max_id", 0)
+			, (IJST_TPRI(Str), max_id_str, "max_id_str", 0)
+			, (IJST_TPRI(Str), next_results, "next_results", 0)
+			, (IJST_TPRI(Str), query, "query", 0)
+			, (IJST_TPRI(Str), refresh_url, "refresh_url", 0)
+			, (IJST_TPRI(Int), count, "count", 0)
+			, (IJST_TPRI(UInt64), since_id, "since_id", 0)
+			, (IJST_TPRI(Str), since_id_str, "since_id_str", 0)
+	)
+
+	IJST_DEFINE_STRUCT(
+			StTwitter
+			, (IJST_TVEC(IJST_TOBJ(StStatus)), statuses, "statuses", 0)
+			, (IJST_TOBJ(StSearchMetaData), search_metadata, "search_metadata", 0)
+	)
+}
+typedef twitter::StTwitter StTwitter;
 
 IJST_DEFINE_STRUCT(
 		StCommon
@@ -115,6 +233,20 @@ public:
 };
 
 
+template<typename T>
+ijstParseResultBase* GetParseResult(const char* json, size_t length)
+{
+	T* val = new T();
+	string strErrMsg;
+	int ret = val->_.Deserialize(json, length, UnknownMode::kKeep, &strErrMsg);
+	if (ret != 0) {
+		cout << "Deserialize error:  "  << strErrMsg << endl;
+		delete val;
+		return 0;
+	}
+	return new ijstParseResult<T>(val);
+}
+
 
 class TEST_CLASS : public TestBase {
 public:
@@ -125,15 +257,20 @@ public:
 
 #if TEST_PARSE
 	virtual ParseResultBase* Parse(const char* json, size_t length, const char* case_name) const {
-		(void)length;
-		StCommon* val = new StCommon();
-		int ret = val->_.Deserialize(json, length);
-		if (ret != 0) {
-			delete val;
-			return 0;
+		cout << case_name << endl;
+		if (strcmp(case_name, "canada.json") == 0) {
+			return GetParseResult<StCanada>(json, length);
 		}
-
-		return new ijstParseResult<StCommon>(val);
+		else if (strcmp(case_name, "twitter.json") == 0) {
+			return GetParseResult<StTwitter>(json, length);
+		}
+		else if(strcmp(case_name, "citm_catalog.json") == 0) {
+			// Use common as a reference
+			return GetParseResult<StCommon>(json, length);
+		}
+		else {
+			return GetParseResult<StCommon>(json, length);
+		}
 	}
 #endif
 
